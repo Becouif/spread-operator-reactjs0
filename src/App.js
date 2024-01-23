7c9fd00cac14fcaba3e9504e377e34f1fdc4d259
becouif <lovguya@gmail.com>
2024-01-23 13:35:26 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [inputText,setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event){
+    const newItem = event.target.value;
+    setInputText(newItem);
+  }
+
+  function addItems(){
+    setItems((prevItem) =>{
+      return [...prevItem, inputText];
+    })
+    // to empty the input after adding setInputText to empty 
+    setInputText("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItems}>
+          <span>Add</span>
+        </button>
+      </div>
+      
+
+      <div>
+        <ul>
+        { 
+          items.map((todoItem)=> {
+            return <li>{todoItem}</li>;
+        })
+
+        }
+          
+        </ul>
+      </div>
     </div>
   );
 }
